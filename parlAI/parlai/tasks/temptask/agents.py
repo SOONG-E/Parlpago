352d078f5510becd42f281ae7317e9c5dc3b8ff5
sumin9366 <68092168+sumin9366@users.noreply.github.com>
2021-06-10 15:14:52 +0900

    Add files via upload

---
+++ parlAI/parlai/tasks/temptask/agents.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+import json
+from parlai.core.teachers import FixedDialogTeacher
+from parlai.core.teachers import ParlAIDialogTeacher
+from parlai.utils.io import PathManager
+from .build import build
+import copy
+
+def _path(opt):
+    build(opt)
+    suffix = ''
+    dt = opt['datatype'].split(':')[0]
+    if dt == 'train':
+        suffix = 'train'
+    elif dt == 'test':
+        suffix = 'test'
+    elif dt == 'valid':
+        suffix = 'valid'
+    return os.path.join(
+        opt['datapath'],
+        'example',
+        '_{suffix}.txt'.format(suffix=suffix),
+    )
+class DefaultTeacher(ParlAIDialogTeacher):
+    def __init__(self, opt, shared=None):
+        build(opt)
+        opt = copy.deepcopy(opt)
+        opt['datafile'] = _path(opt)
+        # get datafile
+        opt['parlaidialogteacher_datafile'] = _path(opt)
+
+        super().__init__(opt, shared)
---
+++ parlAI/parlai/tasks/temptask/build.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+# Download and build the data if it does not exist.
+
+from parlai.core.build_data import DownloadableFile
+import parlai.core.build_data as build_data
+import os
+
+RESOURCES = [
+    DownloadableFile(
+        '1uwht2NhMKiBLyBtPQgabfjOx7hMjtibD',
+        'example5.zip',  
+        '7cc958399ad7c9c6120f54cef548158ef837d648ec5a1077792c6615c9d517f6',
+        from_google=True,
+    )
+]
+
+def build(opt):
+    # get path to data directory
+    dpath = os.path.join(opt['datapath'], 'example')
+    # define version if any
+    version = None
+
+    # check if data had been previously built
+    if not build_data.built(dpath, version_string=version):
+        print('[building data: ' + dpath + ']')
+
+        # make a clean directory if needed
+        if build_data.built(dpath):
+            # an older version exists, so remove these outdated files.
+            build_data.remove_dir(dpath)
+        build_data.make_dir(dpath)
+
+        # download the data.
+        for downloadable_file in RESOURCES:
+            downloadable_file.download_file(dpath)
+
+        # mark the data as built
+        build_data.mark_done(dpath, version_string=version)
